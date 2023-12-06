ae2fb66bc6f3df063405ca06c5a0400e6597dc56
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-07 11:55:03 +1300

    docs: Provided JavaScript Docs For Bullet

--- src/scripts/Bullet.js
+++ src/scripts/Bullet.js
@@ -1,4 +1,16 @@
 class Bullet extends Phaser.GameObjects.Ellipse {
+  /**
+   * Initialization of the bullet object.
+   *
+   * @param {Object} scene - The game's current scene.
+   * @param {number} x - The x coordinate of the bullet.
+   * @param {number} y - The y coordinate of the bullet.
+   * @param {number} mouseX - The x coordinate of the mouse.
+   * @param {number} mouseY - The y coordinate of the mouse.
+   * @param {Function} removeBullet - Callback remove bullet object function.
+   * @param {number} radius - The radius of the bullet object.
+   * @param {number} color - The color of the bullet.
+   */
   constructor(scene, x, y, mouseX, mouseY, removeBullet, radius, color) {
     super(scene, x, y, radius, radius, color);
 
@@ -19,13 +31,23 @@ class Bullet extends Phaser.GameObjects.Ellipse {
     scene.physics.add.existing(this);
   }
 
+  /**
+   * Updates the bullet.
+   *
+   * @return {void} Nothing is returned.
+   */
   update() {
-    this.body.setVelocityX(this.speed * this.dx / this.normalization);
-    this.body.setVelocityY(this.speed * this.dy / this.normalization);
+    this.body.setVelocityX((this.speed * this.dx) / this.normalization);
+    this.body.setVelocityY((this.speed * this.dy) / this.normalization);
 
-    if (this.x < this.bounds.left || this.y < this.bounds.top || this.x > this.bounds.right || this.y > this.bounds.bottom) {
+    if (
+      this.x < this.bounds.left ||
+      this.y < this.bounds.top ||
+      this.x > this.bounds.right ||
+      this.y > this.bounds.bottom
+    ) {
       this.removeBullet(this);
-      this.destroy()
+      this.destroy();
     }
   }
 }
