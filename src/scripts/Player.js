66d6db14e1a93f2a2ac1a9b431b012af1e54107c
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-07 12:03:57 +1300

    docs: Updated JavaScript Docs

--- src/scripts/Player.js
+++ src/scripts/Player.js
@@ -1,4 +1,15 @@
+// eslint-disable-next-line no-unused-vars
 class Player extends Phaser.GameObjects.Ellipse {
+  /**
+   * Initialization of the player object.
+   *
+   * @param {*} scene - The game's current scene.
+   * @param {*} x - The x coordinate of the player.
+   * @param {*} y - The x coordinate of the player.
+   * @param {*} radius - The radius of the player object.
+   * @param {*} color - The color of the player.
+   * @return {void} Nothing is returned.
+   */
   constructor(scene, x, y, radius, color) {
     super(scene, x, y, radius, radius, color);
 
@@ -16,10 +27,24 @@ class Player extends Phaser.GameObjects.Ellipse {
     this.bullets = [];
   }
 
+  /**
+   * Removes the bullet for the list of bullets.
+   *
+   * @param {Object} bullet - The bullet to be removed from the list.
+   * @return {void} Nothing is returned.
+   */
   removeBullet(bullet) {
-    this.bullets = this.bullets.filter(b => b !== bullet);
+    this.bullets = this.bullets.filter((b) => b !== bullet);
   }
 
+  /**
+   * Updates the Player.
+   *
+   * @param {Object} keys - The keyboard input.
+   * @param {Object} cursors - The arrow keys.
+   * @param {Object} mouse - The mouse input.
+   * @return {void} Nothing is returned.
+   */
   update(keys, cursors, mouse) {
     this.body.setVelocityX(0);
     this.body.setVelocityY(0);
@@ -39,18 +64,42 @@ class Player extends Phaser.GameObjects.Ellipse {
     this.updateBullets(mouse);
   }
 
+  /**
+   * Update the arm of the player.
+   *
+   * @param {Object} mouse - The mouse input.
+   * @return {void} Nothing is returned.
+   */
   updateArm(mouse) {
     const theta = Utility.getTheta(this.x, mouse.worldX, this.y, mouse.worldY);
 
     const dr = this.radius - this.arm.radius;
 
-    this.arm.x = this.x + (dr * Math.cos(theta));
-    this.arm.y = this.y + (dr * Math.sin(theta));
+    this.arm.x = this.x + dr * Math.cos(theta);
+    this.arm.y = this.y + dr * Math.sin(theta);
   }
 
+  /**
+   * Update the bullets in the list of bullets.
+   *
+   * @param {Object} mouse - The mouse input.
+   * @return {void} Nothing is returned.
+   */
   updateBullets(mouse) {
     if (mouse.leftButtonDown() && !this.mousePressed) {
-      this.bullets.push(new Bullet(this.scene, this.arm.x, this.arm.y, mouse.x, mouse.y, this.removeBullet.bind(this), 5, 0xffffff));
+      this.bullets.push(
+        new Bullet(
+          this.scene,
+          this.arm.x,
+          this.arm.y,
+          mouse.x,
+          mouse.y,
+          this.removeBullet.bind(this),
+          5,
+          0xffffff,
+        ),
+      );
+
       this.mousePressed = true;
     }
 
--- src/scripts/Utility.js
+++ src/scripts/Utility.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 class Utility {
   /**
    * Calculates the angle in radians between two points.
