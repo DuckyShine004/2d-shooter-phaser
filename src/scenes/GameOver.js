0523d9be5b727dde210fa43e3c046f31413d1fab
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-10 18:10:40 +1300

    feat: Added Main Menu Title

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -11,18 +11,12 @@ class GameOverScene extends BaseScene {
     this.initializeMusic('game_over_music');
     this.initializeUI(this, 'default', 'game_over');
 
-    this.time.delayedCall(
-      1000,
-      () => {
-        this.add
-          .text(WINDOW_WIDTH * 0.5, WINDOW_HEIGHT * 0.2, 'Game Over', {
-            fontFamily: 'GameOver',
-            fontSize: '60px',
-            fill: '#ffffff',
-          })
-          .setOrigin(0.5, 0.5);
-      },
-      this,
-    );
+    this.add
+      .text(WINDOW_WIDTH * 0.5, WINDOW_HEIGHT * 0.2, 'Game Over', {
+        fontFamily: 'GameOver',
+        fontSize: '60px',
+        fill: '#ffffff',
+      })
+      .setOrigin(0.5, 0.5);
   }
 }
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -10,5 +10,13 @@ class MenuScene extends BaseScene {
 
   create() {
     this.initializeUI(this, 'default', 'main_menu');
+
+    this.add
+      .text(WINDOW_WIDTH * 0.5, WINDOW_HEIGHT * 0.2, 'Main Menu', {
+        fontFamily: 'GameOver',
+        fontSize: '60px',
+        fill: '#000000',
+      })
+      .setOrigin(0.5, 0.5);
   }
 }
