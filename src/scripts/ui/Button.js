f84e829004841e74095cf6c5780cfe32182c8b19
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-11 00:42:38 +1300

    docs: Added Documentation For UI Classes

--- src/scripts/ui/Button.js
+++ src/scripts/ui/Button.js
@@ -1,5 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 class Button extends Phaser.GameObjects.Image {
+  /**
+   * Initialization of the button object.
+   *
+   * @param {Object} scene - The current scene.
+   * @param {number} x - The x coordinate of the button.
+   * @param {number} y - The y coordinate of the button.
+   * @param {string} normal - The state of the button.
+   * @param {string} hover - The state of the button.
+   * @param {string} click - The state of the button.
+   * @param {Function} callback - the callback function when the button is pressed.
+   * @return {void} Nothing is returned.
+   */
   constructor(scene, x, y, normal, hover, click, callback) {
     super(scene, x, y, normal);
 
