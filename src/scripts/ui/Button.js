bcaafafda25864ccae4f138839a82f7ee872086d
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-10 23:32:00 +1300

    refactor: Refactored UI Elements

---
+++ src/scripts/ui/Button.js
@@ -0,0 +1,28 @@
+// eslint-disable-next-line no-unused-vars
+class Button extends Phaser.GameObjects.Image {
+  constructor(scene, x, y, normal, hover, click, callback) {
+    super(scene, x, y, normal);
+
+    this.normal = normal;
+    this.hover = hover;
+    this.click = click;
+
+    this.setScale(0.75);
+
+    this.setInteractive()
+      .on('pointerdown', () => {
+        this.setTexture(this.click);
+        callback();
+      })
+      .on('pointerover', () => {
+        this.setTexture(this.hover);
+      })
+      .on('pointerout', () => {
+        this.setTexture(this.normal);
+      });
+
+    scene.add.existing(this);
+
+    this.setDepth(1);
+  }
+}
